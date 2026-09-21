a6b4d0f0f6239825555092a873174954ca104069
Aman <amankumar105541@gmail.com>
2026-09-21 14:24:44 +0530

    Replace header text with logo image in header component

--- src/components/header.py
+++ src/components/header.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
 def header_home():
-    st.header('ClassVision...')
+    logo_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQT9DJverBiwBhROsiQsneJ4ch13XFESHIQRZVmIs_-w&s=10"
+    st.markdown(f"""
+            <div>
+                <img src='{logo_url}', height=100px; />
+            </div>
+
+            """,unsafe_allow_html=True)
