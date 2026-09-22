7c7d2ac7c3970932c8db93c2fa66833bc86fb652
Aman <amankumar105541@gmail.com>
2026-09-22 14:52:22 +0530

    Implement teacher login validation and add face model loading function

--- src/pipelines/face_pipeline.py
+++ src/pipelines/face_pipeline.py
@@ -5,3 +5,14 @@ from sklearn.svm import SVC
 import streamlit as st
 from src.database.db import get_all_students
 
+# face Image -> face detector(dlib) -> shape predictor(sp) (Landmarks) -> resnet(facerecog)(embedding(128D)) -> SVC -> student id
+
+@st.cache_resource  ## loads the model only once
+def load_dlib_models():
+    detector=dlib.get_frontal_face_detector()
+
+    sp=dlib.shape_predictor(face_recognition_models.pose_predictor_model_location())
+
+    facerec=dlib.face_recognition_model_v1(face_recognition_models.face_recognition_model_location())
+
+    return detector,sp,facerec
