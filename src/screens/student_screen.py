c1431a481ef227a18cdc35ab84363ff2b5e6614f
Aman <amankumar105541@gmail.com>
2026-09-23 15:49:29 +0530

    Add account creation feature with face and voice enrollment in student screen

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -7,8 +7,9 @@ from PIL import Image
 import numpy as np
 import time
 
-from src.pipelines.face_pipeline import predict_attendance
-from src.database.db import get_all_students
+from src.pipelines.face_pipeline import predict_attendance,get_face_embeddings, train_classifier
+from src.pipelines.voice_pipeline import get_voice_embedding
+from src.database.db import get_all_students, create_student
 
 def student_dashboard():
     st.header('ding ding ding..')
@@ -74,6 +75,35 @@ def student_screen():
                 audio_data = st.audio_input('Record a short phrase like I am present, My name is Akash.')
             except Exception:
                 st.error('Audio Data failed!')
+
+            if st.button('Create Account', type='primary'):
+                if new_name:
+                    with st.spinner('Creating profile..'):
+                        img = np.array(Image.open(photo_source))
+                        encodings= get_face_embeddings(img)
+                        if encodings:
+                            face_emb = encodings[0].tolist()
+
+                            voice_emb = None
+                            if audio_data:
+                                voice_emb = get_voice_embedding(audio_data.read())
+
+                            response_data = create_student(new_name, face_embedding=face_emb, voice_embedding=voice_emb)
+
+                            if response_data:
+                                train_classifier()
+                                st.session_state.is_logged_in = True
+                                st.session_state.user_role = 'student'
+                                st.session_state.student_data = response_data[0]
+                                st.toast(f'Profile Created! Hi {new_name}!')
+                                time.sleep(1)
+                                st.rerun()
+                        else:
+                            st.error('Couldnt capture your facial features for registration')
+
+                else:
+                    st.warning('Do You have any Name')
+
 
 
 
