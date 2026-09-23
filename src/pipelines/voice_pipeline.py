717701b8a0b10722cbd2894b6cf218a01a20d2d1
Aman <amankumar105541@gmail.com>
2026-09-23 15:00:58 +0530

    get_voice_embedding for clarity on audio processing steps

--- src/pipelines/voice_pipeline.py
+++ src/pipelines/voice_pipeline.py
@@ -14,9 +14,9 @@ def get_voice_embedding(audio_bytes):
     try:
         encoder = load_voice_encoder()
 
-        audio, sr = librosa.load(io.BytesIO(audio_bytes), sr=16000)
+        audio, sr = librosa.load(io.BytesIO(audio_bytes), sr=16000)   #audio bytes into audio data   ##16k sample/second
         wav = preprocess_wav(audio)
-        embedding = encoder.embed_utterance(wav)
+        embedding = encoder.embed_utterance(wav)  #256D embedding
         return embedding.tolist()
     except Exception as e:
         st.error('Voice recog error')
