1bf2b6e8428fdd6db682ddccd74f378ede9547e1
Aman <amankumar105541@gmail.com>
2026-09-25 13:01:56 +0530

    Add missing imports for student subjects and attendance retrieval

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -9,7 +9,7 @@ import time
 
 from src.pipelines.face_pipeline import predict_attendance,get_face_embeddings, train_classifier
 from src.pipelines.voice_pipeline import get_voice_embedding
-from src.database.db import get_all_students, create_student
+from src.database.db import get_all_students, create_student, get_student_subjects,get_student_attendance
 from src.components.dialog_enroll import enroll_dialog
 
 def student_dashboard():
