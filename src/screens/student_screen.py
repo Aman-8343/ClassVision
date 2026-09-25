e173628f00df56867392fc57c41a13fce1646ada
Aman <amankumar105541@gmail.com>
2026-09-25 13:47:59 +0530

    Add attendance statistics and unenrollment functionality to student dashboard

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -9,8 +9,9 @@ import time
 
 from src.pipelines.face_pipeline import predict_attendance,get_face_embeddings, train_classifier
 from src.pipelines.voice_pipeline import get_voice_embedding
-from src.database.db import get_all_students, create_student, get_student_subjects,get_student_attendance
-from src.components.dialog_enroll import enroll_dialog
+from src.database.db import get_all_students, create_student, get_student_subjects,get_student_attendance ,unenroll_student_to_subject
+from src.components.dialog_enroll import enroll_dialog 
+from src.components.subject_card import subject_card
 
 def student_dashboard():
     student_data = st.session_state.student_data
@@ -37,6 +38,47 @@ def student_dashboard():
     with st.spinner('Loading your enrolled subjects..'):
         subjects = get_student_subjects(student_id)
         logs = get_student_attendance(student_id)
+
+    stats_map = {}
+
+    for log in logs:
+        sid = log['subject_id']
+
+        if sid not in stats_map:
+            stats_map[sid] = {"total":0, "attended": 0}
+
+        stats_map[sid]['total'] +=1   #total class
+
+        if log.get('is_present'):
+            stats_map[sid]['attended'] += 1   #if present
+
+
+    cols = st.columns(2)
+    for i, sub_node in enumerate(subjects):
+        sub = sub_node['subjects']
+        sid = sub['subject_id']
+
+        stats = stats_map.get(sid,{"total":0, "attended": 0} )
+        def unenroll_button():
+                if st.button("Unenroll from tihs course", type='tertiary', width='stretch', icon=':material/delete_forever:'):
+                    unenroll_student_to_subject(student_id, sid)
+                    st.toast(f'Unenrolled from {sub['name']} successfully!')
+                    st.rerun()
+
+        with cols[i % 2]:
+
+            subject_card(
+                name = sub['name'],
+                code =sub['subject_code'],
+                section = sub['section'],
+                stats = [
+                    ('📅', 'Total', stats['total']),
+                    ('✅', 'Attended', stats['attended']),
+                ],
+                footer_callback=unenroll_button
+            )
+    footer_dashboard()
+
 
 def student_screen():
     style_background_dashboard()
