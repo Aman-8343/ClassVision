46c7714146bbd46c82aa82faa932242d0e776d12
Aman <amankumar105541@gmail.com>
2026-09-25 22:24:42 +0530

    Sometimes I forgot to use same function name at the time of import

--- src/components/dialog_attendance_results.py
+++ src/components/dialog_attendance_results.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 from src.database.db import create_attendance
 
-def show_attendance_result(df, logs):
+@st.dialog("Attendance Reports")
+def attendance_result_dialog(df, logs):
     st.write('Please review attendance before confirming.')
     st.dataframe(df, hide_index=True, width='stretch')
 
@@ -25,7 +26,3 @@ def show_attendance_result(df, logs):
             except Exception as e:
                 st.error('Sync failed!')
 
-
-@st.dialog("Attendance Reports")
-def attendance_result_dialog(df, logs):
-    show_attendance_result(df, logs)
