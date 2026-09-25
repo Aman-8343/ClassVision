0e077d53f8bea19ede2c0464146a53b786747319
Aman <amankumar105541@gmail.com>
2026-09-25 12:32:27 +0530

    Add dialog component for enrolling in subjects

---
+++ src/components/dialog_enroll.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+@st.dialog("Enroll in Subject")
+def enroll_dialog():
+    st.write('Enter the subject code provided by your teacher to enroll')
+    join_code = st.text_input('Subject Code', placeholder='Eg. CS101')
