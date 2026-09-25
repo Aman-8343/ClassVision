097f0389d0ac525e86a59676cb3d71db478328e8
Aman <amankumar105541@gmail.com>
2026-09-25 15:48:10 +0530

    Add dialog for capturing or uploading classroom photos

---
+++ src/components/dialog_add_photo.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from PIL import Image
+
+
+@st.dialog("Capture or upload photos")
+def add_photos_dialog():
+
+    st.write('Add classroom photos to scan for attendance')
+
+    if 'photo_tab' not in st.session_state:
+        st.session_state.photo_tab = 'camera'
+
+    t1, t2 = st.columns(2)
+
+    with t1:
+        type_camera = "primary" if st.session_state.photo_tab == 'camera' else 'tertiary'
+        if st.button('Camera', type=type_camera, width='stretch'):
+            st.session_state.photo_tab = 'camera'
+
+
+
+    with t2:
+        type_upload = "primary" if st.session_state.photo_tab == 'upload' else 'tertiary'
+        if st.button('Upload photos', type=type_upload, width='stretch'):
+            st.session_state.photo_tab = 'upload'
+
