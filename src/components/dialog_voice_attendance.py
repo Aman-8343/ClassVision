a47e427312a1487191ce7d01d7e7e61f790c5e21
Aman <amankumar105541@gmail.com>
2026-09-26 13:28:56 +0530

    Enhance voice attendance dialog with audio processing and error handling

--- src/components/dialog_voice_attendance.py
+++ src/components/dialog_voice_attendance.py
@@ -1,9 +1,27 @@
 import streamlit as st
+from src.pipelines.voice_pipeline import process_bulk_audio
+from src.database.config import supabase
+import pandas as pd
 
 def voice_attendance_dialog(selected_subject_id):
     st.write('Record audio of students saying I am present. Then AI will recognize the students')
 
-
     audio_data = None
-
     audio_data = st.audio_input("Record classroom audio")
+
+    if st.button('Analyze Audio', width='stretch', type='primary'):
+        with st.spinner('Prcessing Audio data'):
+            enrolled_res = supabase.table('subject_students').select("*, students(*)").eq('subject_id',selected_subject_id ).execute()
+            enrolled_students = enrolled_res.data
+
+            if not enrolled_students:
+                st.warning('No students enrolled in this course')
+                return
+            candidates_dict = {
+                s['students']['student_id'] : s['students']['voice_embedding'] 
+                for s in enrolled_students if s['students'].get('voice_embedding')
+            }
+
+            if not candidates_dict:
+                st.error('No enrolled students have voice profiles registerd')
+                return
