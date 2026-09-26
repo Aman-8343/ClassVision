38a0cb7f57bfc02f28f6b155da3671e120939449
Aman <amankumar105541@gmail.com>
2026-09-26 14:09:22 +0530

    style changes

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -55,10 +55,11 @@ def style_base_layout():
                 
 
             h2 {
-                font-family: 'Climate Crisis', sans-serif !important;
+                font-family: 'Roboto Mono', sans-serif !important;
                 font-size: 2rem !important;
                 line-height:0.9 !important;
                 margin-bottom:0rem !important;
+                color: black !important
             }
                 
             h3, h4, p {
