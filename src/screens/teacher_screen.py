e9c3f953b28361a2a521ec09af3e22f90081e5fe
Aman <amankumar105541@gmail.com>
2026-09-26 14:00:55 +0530

    Add attendance records retrieval for teachers in attendance tab

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -17,6 +17,7 @@ from datetime import datetime
 import pandas as pd
 from src.components.dialog_attendance_results import attendance_result_dialog
 from src.components.dialog_voice_attendance import voice_attendance_dialog
+from src.database.db import get_attendance_for_teacher
 
 
 def teacher_screen():
@@ -212,7 +213,27 @@ def teacher_tab_manage_subjects():
         st.info("NO SUBJECTS FOUND. CREATE ONE ABOVE")
     
 def teacher_tab_attendance_records():
-    st.subheader("Attendance Records")
+    st.header('Attendance Records')
+
+    teacher_id = st.session_state.teacher_data['teacher_id']
+
+    records = get_attendance_for_teacher(teacher_id)
+
+    if not records:
+        return
+    
+    data = []
+
+    for r in records:
+        ts = r.get('timestamp')
+
+        data.append({
+            "ts_group": ts.split(".")[0] if ts else None,
+            "Time": datetime.fromisoformat(ts).strftime("%Y-%m-%d %I:%M %p") if ts else "N'A",
+            "Subject": r['subjects']['name'],
+            "Subject Code":r['subjects']['subject_code'],
+            "is_present": bool(r.get('is_present', False))
+        })
     
 
 
